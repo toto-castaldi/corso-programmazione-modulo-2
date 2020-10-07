e7d03a20df4fdef64c7403bbe3bdfa316e2b5cba
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2020-10-07 17:41:21 +0200

    lezione live del 7/10/2020

--- sketch.js
+++ sketch.js
@@ -19,6 +19,9 @@ let platforms;
 let player;
 let keyboard;
 let stars;
+let scoreText;
+let scoreValue;
+let bombs;
 
 function scenePreload() {
     console.log("preload");
@@ -33,6 +36,9 @@ function scenePreload() {
 
 function sceneCreate() {
     console.log("create");
+
+    scoreValue = 0;
+
     this.add.image(400, 300, "sky");
 
     platforms = this.physics.add.staticGroup();
@@ -49,14 +55,64 @@ function sceneCreate() {
     player.setBounce(0.3);
 
     stars = this.physics.add.group({
-        key : "star",
-        repeat : 10,
-        setXY : {
-            x : 20,
-            y : 0,
-            stepX : 80
+        key: "star",
+        repeat: 2,
+        setXY: {
+            x: 20,
+            y: 0,
+            stepX: 70
         }
     });
+
+    this.physics.add.collider(stars, platforms);
+
+    stars.children.iterate((star) => {
+        star.setBounceY(Phaser.Math.FloatBetween(0.2, 0.5));
+    });
+
+    bombs = this.physics.add.group();
+    this.physics.add.collider(bombs, platforms);
+
+    this.physics.add.overlap(player, stars, (p, s) => {
+        s.disableBody(true, true);
+        scoreValue += 10;
+        scoreText.setText("Score : " + scoreValue);
+
+        
+
+        if (stars.countActive() == 0) {
+            stars.children.iterate((star) => {
+                star.enableBody(true, star.x, 0, true, true);
+            });
+
+            let bomb = bombs.create(400, 16, "bomb");
+            bomb.setBounce(1);
+            bomb.setCollideWorldBounds(true);
+            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        }
+    });
+
+    scoreText = this.add.text(16, 16, "Score : " + scoreValue, { fontSize: "32px", fill: "black" });
+
+    this.anims.create({
+        key: "left",
+        frames: [{ key: "dude", frame: 0 }, { key: "dude", frame: 1 }, { key: "dude", frame: 2 }, { key: "dude", frame: 3 }],
+        frameRate: 10
+    });
+
+    this.anims.create({
+        key: "right",
+        frames: [{ key: "dude", frame: 5 }, { key: "dude", frame: 6 }, { key: "dude", frame: 7 }, { key: "dude", frame: 8 }],
+        frameRate: 10
+    });
+
+    this.anims.create({
+        key: "center",
+        frames: [{ key: "dude", frame: 4 }],
+        frameRate: 20
+    });
+
+
 }
 
 function sceneUpdate() {
@@ -65,14 +121,22 @@ function sceneUpdate() {
     //movimento orizzontale 
     if (keyboard.left.isDown) {
         player.setVelocityX(-160);
+        player.anims.play("left", true);
     } else if (keyboard.right.isDown) {
         player.setVelocityX(+160);
+        player.anims.play("right", true);
     } else {
+        player.anims.play("center");
         player.setVelocityX(0);
     }
 
     //salto
     if (keyboard.up.isDown && player.body.touching.down) {
         player.setVelocityY(-400);
+    }
+
+    //discesa rapida
+    if (keyboard.down.isDown) {
+        player.setVelocityY(400);
     }
 }
